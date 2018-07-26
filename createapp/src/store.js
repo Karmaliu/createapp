eec12e8d218b625412b6810ec0516f40e5c06c2c
Karmaliu <2134515368@qq.com>
2018-07-26 14:18:54 +0800

    加入saga 操作

--- createapp/src/store.js
+++ createapp/src/store.js
@@ -1,8 +1,29 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import mySaga from './pages/counter/sagas';
 import counterR from './pages/counter/reducers/index';
-//里面放入个个页面的reducer 和 state
+
+
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware()
+// 合并reducer
 const reducer = combineReducers({
     counter: counterR.counterReducer
 })
 
-export const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const store = createStore(
+    reducer,
+    window.devToolsExtension ? window.devToolsExtension() : undefined,    // dev-tools
+    applyMiddleware(sagaMiddleware)                 // 中间件，加载sagaMiddleware
+)
+
+
+// then run the saga
+sagaMiddleware.run(mySaga);
+
+
+
+
+
+
+
