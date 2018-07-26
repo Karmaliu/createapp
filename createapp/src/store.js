31921da930d61c159f3147bad64c30ac980df8e4
Karmaliu <2134515368@qq.com>
2018-07-26 11:18:31 +0800

    加入了todoloov 删除了layout中的children

--- createapp/src/store.js
+++ createapp/src/store.js
@@ -6,4 +6,4 @@ const reducer = combineReducers({
 })
 
 
-export const store = createStore(reducer);
+export const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
