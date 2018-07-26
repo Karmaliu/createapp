50d3d1d729bf463ac99f37848498f9254b637b3e
Karmaliu <2134515368@qq.com>
2018-07-26 18:50:54 +0800

    fetch 跨域代理

--- createapp/src/pages/counter/server/counter.js
+++ createapp/src/pages/counter/server/counter.js
@@ -1,5 +1,5 @@
 import request from '../../../utils/request';
 
 export function queryWeather(city) {
-    return request(`/mmm/air/cityair?city=${city}&key=31876d06cf5cad066b21655aed7b0d9a`);
+    return request(`/api/air/cityair?city=shijiazhuang&key=31876d06cf5cad066b21655aed7b0d9a`);
 }
